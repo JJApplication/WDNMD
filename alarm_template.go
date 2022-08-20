eb9732b71a79179767e0f73eea42209bbb6b8144
landers <liaorenj@gmail.com>
2022-08-20 23:52:54 +0800

    去除尾部换行

--- alarm_template.go
+++ alarm_template.go
@@ -17,6 +17,6 @@ const (
 )
 
 const (
-	SystemInfoTemplateMongo  = "环境信息\n操作系统: %s\n系统家族: %s\n系统版本: %s\n内核版本: %s\n上次启动时间: %s\nCPU核心数: %d\nCPU使用率: %v\n内存使用率: %v\n可用内存: %v\n运行进程数: %d\n"
-	SystemAlertTemplateMongo = "系统告警\n内存占用达到阈值\n内存占用: %f%%\n空闲内存: %f bytes\n"
+	SystemInfoTemplateMongo  = "环境信息\n操作系统: %s\n系统家族: %s\n系统版本: %s\n内核版本: %s\n上次启动时间: %s\nCPU核心数: %d\nCPU使用率: %v\n内存使用率: %v\n可用内存: %v\n运行进程数: %d"
+	SystemAlertTemplateMongo = "系统告警\n内存占用达到阈值\n内存占用: %f%%\n空闲内存: %f bytes"
 )
